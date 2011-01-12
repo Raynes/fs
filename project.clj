26d62eeb898e335d967483cfb5a88fd7afb87882
Miki Tebeka <miki.tebeka@gmail.com>
2011-01-12 12:41:13 -0800

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fs "0.1.2-SNAPSHOT"
+(defproject fs "0.2.0-SNAPSHOT"
   :description "File system utilities for clojure"
   :url "https://bitbucket.org/tebeka/fs/src"
   :dependencies [[org.clojure/clojure "1.2.0"]])
