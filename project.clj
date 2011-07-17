59689c71ee08143581919c9ddaf7df0e40289e32
Miki Tebeka <miki.tebeka@gmail.com>
2011-07-17 07:49:39 -0700

    0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fs "0.7.2"
+(defproject fs "0.8.0"
   :description "File system utilities for clojure"
   :url "https://bitbucket.org/tebeka/fs/src"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
