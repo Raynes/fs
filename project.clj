7178d4fa9f9d14122a2870b79fb306a27e43ff09
Anthony Grimes <i@raynes.me>
2011-12-10 18:03:51 -0600

    Change project.clj to reflect move.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fs "0.11.0"
+(defproject fs "1.0.0-SNAPSHOT"
   :description "File system utilities for clojure"
-  :url "https://bitbucket.org/tebeka/fs/src"
+  :url "https://github.com/Raynes/fs"
   :dependencies [[org.clojure/clojure "1.3.0"]])
