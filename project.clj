0e608b9384d89450b326e63fc2f160624459073a
Anthony Grimes <i@raynes.me>
2012-01-18 19:25:31 -0600

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fs "1.1.1"
+(defproject fs "1.1.2"
   :description "File system utilities for clojure"
   :url "https://github.com/Raynes/fs"
   :dependencies [[org.clojure/clojure "1.3.0"]
