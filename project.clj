b6a4a030bf00283746079250224a6ceb0e8c1010
Anthony Grimes <i@raynes.me>
2012-06-10 16:42:25 -0500

    Test against multiple Clojure versions.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject fs "1.1.2"
   :description "File system utilities for clojure"
   :url "https://github.com/Raynes/fs"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.commons/commons-compress "1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}
+             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
