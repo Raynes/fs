06046d75c49578bda34130504afddc76d64d51bc
Anthony Grimes <i@raynes.me>
2012-08-04 17:12:06 -0500

    Use testall instead of test-all.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "https://github.com/Raynes/fs"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.commons/commons-compress "1.3"]]
-  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
