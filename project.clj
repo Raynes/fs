2a0fad5e299277b8c03ed9e204a72d9511466632
Anthony Grimes <i@raynes.me>
2013-04-11 15:40:38 -0700

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.raynes/fs "1.4.1"
+(defproject me.raynes/fs "1.4.2"
   :description "File system utilities for clojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
