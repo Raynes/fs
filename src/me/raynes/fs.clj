983388bf8f1537a3358a5f2fd0ca9235a03426ac
Nicola Mometto <brobronsa@gmail.com>
2014-03-31 19:13:29 +0200

    fix typo

--- src/me/raynes/fs.clj
+++ src/me/raynes/fs.clj
@@ -153,7 +153,7 @@
       (extend-protocol io/Coercions
        Path
        (as-file [this] (.toFile this))
-       (as-url [this] (. this (toFile) (toUrl))))
+       (as-url [this] (. this (toFile) (toURL))))
 
       (defn- ^Path as-path
         "Convert path to a java.nio.file.Path.
